44afbd6b21c641c1d96d41fcb686dd91a44a4654
Tylor <Zapblumar@gmail.com>
2020-10-10 18:45:11 -0700

    completed the ReadMe page

--- README.md
+++ README.md
@@ -1,16 +1,56 @@
--The requirments:
-GIVEN I need a new, secure password
-WHEN I click the button to generate a password
+# Password-Gen
+
+It is a random password generator that makes a password from what you selected and the length that you picked or a random length if you put a invalid number or letter.
+
+## Getting Started
+
+My Pasword Gen -
+https://zapblumar.github.io/password-gen/
+
+### Prerequisites
+
+
+```
+GIVEN I need a new, secure password.
+WHEN I click the button to generate a password.
 THEN I am presented with a series of prompts for password criteria
-WHEN prompted for password criteria
-THEN I select which criteria to include in the password
-WHEN prompted for the length of the password
-THEN I choose a length of at least 8 characters and no more than 128 characters
-WHEN prompted for character types to include in the password
-THEN I choose lowercase, uppercase, numeric, and/or special characters
-WHEN I answer each prompt
-THEN my input should be validated and at least one character type should be selected
-WHEN all prompts are answered
-THEN a password is generated that matches the selected criteria
-WHEN the password is generated
-THEN the password is either displayed in an alert or written to the page
+WHEN prompted for password criteria.
+THEN I select which criteria to include in the password.
+WHEN prompted for the length of the password.
+THEN I choose a length of at least 8 characters and no more than 128 characters.
+WHEN prompted for character types to include in the password.
+THEN I choose lowercase, uppercase, numeric, and/or special characters.
+WHEN I answer each prompt.
+THEN my input should be validated and at least one character type should be selected.
+WHEN all prompts are answered.
+THEN a password is generated that matches the selected criteria.
+WHEN the password is generated.
+THEN the password is either displayed in an alert or written to the page.
+```
+
+
+## Running the tests
+
+I ran the password gen and put a random number in it as a test and made sure it didn't fail if they tryed to put anything else in the box.
+
+### Break down into end to end tests
+```
+I made sure that if someone tryed and but a 5 in the box it would not only have 5 units or didnt to anything.
+I tryed to put a word in the box and made sure that wouldn't mess with the outcome.
+```
+
+
+## Built With
+
+* VS Code
+* git
+
+
+
+## Acknowledgments
+
+* the number-gen video
+* some insperation from stack overflow.
+* Jason Whitted
+
+<img src="password-gen.PNG" />
--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // Assignment code here
-//debugger;
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -10,9 +9,7 @@ function writePassword() {
   var passwordLength = window.prompt("enter a digit between 8 to 128 number. If outside parameters, or left blank will make random size!","8");
   if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)){
   var passwordLength =(Math.floor(Math.random() *(129 - 8))) + 8;
-  parseInt(passwordLength);
   }
-  console.log (passwordLength)
   
   // may need more work in the if statement.
 var passwordUppercase = window.confirm("Press OK for uppercase letters?");
@@ -21,45 +18,36 @@ let upperCaseChar = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O"
 
 if(passwordUppercase === true){
   var upperCase = (Math.floor(Math.random()*(26)))
-  parseInt(upperCase);
   passwordUppercase =  upperCaseChar[upperCase];
   generatePassword = (generatePassword + passwordUppercase);
   passCode =  passCode.concat(upperCaseChar);
   passwordLength = passwordLength - 1;
 }
-console.log (passwordUppercase);
 var passwordLowercase = window.confirm("Press OK for lowercase numbers?");
 let lowerCaseChar = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
 if(passwordLowercase === true){
   var lowerCase = (Math.floor(Math.random()*(26)));
-  parseInt(lowerCase);
   passwordLowercase = lowerCaseChar[lowerCase];
   generatePassword = (generatePassword + passwordLowercase);
   passCode = passCode.concat(lowerCaseChar);
   passwordLength = passwordLength - 1;
 }
-console.log(passwordLowercase);
-console.log(generatePassword);
-console.log(passCode);
 var passwordNumber = window.confirm("Press OK for numbers?");
 let numericChar = [ "1","2","3","4","5","6","7","8","9","0"];
 
 if(passwordNumber === true){
   var number = (Math.floor(Math.random()*(10)));
-  parseInt(number);
   passwordNumber = numericChar[number];
   generatePassword = (generatePassword + passwordNumber); 
   passCode = passCode.concat(numericChar);
   passwordLength = passwordLength - 1;
 }
-console.log(passwordNumber);
 var passwordCharacter = window.confirm("Press OK for special characters?");
 let specialChar = [ "!","#","$","%","&","'","(",")","*","+","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~",];
 
 if(passwordCharacter === true){
   var caseArray = (Math.floor(Math.random()*(30)));
-  parseInt(caseArray);
   passwordCharacter = specialChar[caseArray];
   generatePassword = (generatePassword + passwordCharacter);
   passCode = passCode.concat(specialChar);
@@ -71,12 +59,6 @@ for (var n = 0; n < middleLength; n = n + 1) {
    randomCharactor = (Math.floor(Math.random()*(passCode.length)));
   generatePassword = (generatePassword + passCode[randomCharactor]);
 }
-
-
-//console.log(passwordCharacter);
-console.log(generatePassword);
-
-
 
  var password = generatePassword;
   var passwordText = document.querySelector("#password");{
