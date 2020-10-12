f5098a277765f47205240fae020230b8c6b0a648
Tylor <Zapblumar@gmail.com>
2020-10-11 18:02:51 -0700

    got rid of the brackets

--- script.js
+++ script.js
@@ -61,11 +61,11 @@ function writePassword() {
   }
 
   var password = generatePassword;
-  var passwordText = document.querySelector("#password"); {
+  var passwordText = document.querySelector("#password"); 
 
     passwordText.value = password;
 
-  }
+  
 }
 
 // Add event listener to generate button
